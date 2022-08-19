fc4bca49c2dad077bd1cdd0c0569133dae186240
JiaweiGu <42446055+qianbd@users.noreply.github.com>
2022-08-19 17:08:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,7 @@ to_repo_name=os.environ["TO_REPO"]
 def check_issue_contain_label(issue_num,common_label):
     from_repo = gh.get_repo(from_repo_name)
     issue=from_repo.get_issue(issue_num)
-    print(issue)
     for i in issue.labels:
-        print(i)
         if i.name == common_label:
             return True
     return False
@@ -24,10 +22,7 @@ def check_issue_contain_label(issue_num,common_label):
 def send_issue_to_repo(issue_num,to_repo_name):
     from_repo = gh.get_repo(from_repo_name)
     issue=from_repo.get_issue(issue_num)
-    print('issue is ',issue)
     to_repo = gh.get_repo(to_repo_name)
-    print('to repo',to_repo)
-    print('nnn',issue.title,issue.body,issue.labels)
     to_repo.create_issue(title=issue.title,body=issue.body,labels=issue.labels)
 
 
